eea5971bde897e922a63cb96f6d84e965f43387e
lishengmin <shengminfang@foxmail.com>
2025-06-13 14:25:29 +0800

    updata

--- utils/api.js
+++ utils/api.js
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // API配置
-const API_BASE_URL = 'http://120.46.28.146:9001';
+const API_BASE_URL = 'https://120.46.28.146:9000';
 
 // 通用请求方法
 function request(options) {
